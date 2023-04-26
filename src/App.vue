21d148e19f628487c2d100a8cd993a056ca3d4c6
csemiraz <mirazul2013@gmail.com>
2023-04-26 10:54:49 +0600

    Vue Project install

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, asperiores? Necessitatibus obcaecati corporis, atque eaque vero tempora a praesentium voluptas.</p>
+</template>
+
+<style scoped>
+
+</style>
